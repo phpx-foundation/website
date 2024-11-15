f5d2a809766f5fb71ad088a5569df9a11d35c89f
Chris Morrell <inxilpro@users.noreply.github.com>
2024-11-15 17:05:57 -0500

    Center on philly for now :P

--- resources/js/globe.js
+++ resources/js/globe.js
@@ -5,11 +5,23 @@ import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls
 const node = document.getElementById('globe-visualization');
 const data = JSON.parse(node.dataset.points);
 
+const colorInterpolator = t => `rgba(255, 100, 50, ${ 1 - t })`;
+
 const Globe = new ThreeGlobe()
 	.globeImageUrl('https://unpkg.com/three-globe/example/img/earth-night.jpg')
 	.bumpImageUrl('https://unpkg.com/three-globe/example/img/earth-topology.png')
-	.pointsData(data)
-	.pointAltitude('size');
+	.ringsData(data)
+	.ringColor(() => colorInterpolator)
+	.ringMaxRadius(() => 5)
+	.ringPropagationSpeed(() => 1)
+	.ringRepeatPeriod(() => 2000)
+	.labelsData(data)
+	.labelSize(() => 0.8)
+	.labelDotRadius(() => 0.1)
+	.labelColor(() => 'white')
+	.labelText('name');
+
+console.log(data);
 
 // Setup renderer
 const renderer = new THREE.WebGLRenderer();
@@ -26,7 +38,7 @@ scene.add(new THREE.DirectionalLight(0xffffff, 0.6 * Math.PI));
 const camera = new THREE.PerspectiveCamera();
 camera.aspect = node.clientWidth / node.clientHeight;
 camera.updateProjectionMatrix();
-camera.position.z = 180;
+camera.position.z = 150;
 
 // Add camera controls
 const tbControls = new TrackballControls(camera, renderer.domElement);
@@ -34,15 +46,26 @@ tbControls.minDistance = 101;
 tbControls.rotateSpeed = 5;
 tbControls.zoomSpeed = 0.8;
 
-// Position globe
-Globe.rotation.y = THREE.MathUtils.degToRad(data[0].lng);
-Globe.rotation.x = THREE.MathUtils.degToRad(-1 * data[0].lat);
+let last_coords = [null, null];
+
+function show(lat, lng) {
+	const x = THREE.MathUtils.degToRad(lat);
+	const y = THREE.MathUtils.degToRad(-lng);
+	
+	last_coords = [x, y];
+	
+	Globe.rotation.set(x, y, 0, 'XYZ');
+}
 
 // Kick-off renderer
-(function animate() { // IIFE
-	// Frame cycle
+// let centered = false;
+(function animate() {
+	show(data[0].lat, data[0].lng);
+	// Globe.rotation.y += 0.001
+	
 	tbControls.update();
-	Globe.rotation.y += 0.001;
 	renderer.render(scene, camera);
 	requestAnimationFrame(animate);
 })();
+
+// setTimeout(() => console.log(Globe.rotation), 1000);
--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,12 @@ foreach (RootDomains::cases() as $case) {
 	Route::domain($case->value)->group(function() {
 		Route::get('/', function() {
 			return view('world.home', [
-				'points' => Group::query()->select(['latitude', 'longitude'])->toBase()->get()
-					->map(fn($row) => ['lat' => $row->latitude, 'lng' => $row->longitude, 'size' => 0.2])
+				'points' => Group::query()->select(['name', 'latitude', 'longitude'])->get()
+					->map(fn($row) => [
+						'lat' => $row->latitude, 
+						'lng' => $row->longitude,
+						'name' => $row->label(),
+					])
 					->toArray(),
 			]);
 		});
