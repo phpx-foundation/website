07b0b515b4ba3ae8f5bbca7479bed8b45d7e686b
Chris Morrell <inxilpro@users.noreply.github.com>
2024-11-15 20:33:02 -0500

    Use local images

--- resources/js/globe.js
+++ resources/js/globe.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import ThreeGlobe from 'three-globe';
-import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
+// import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
 
 const node = document.getElementById('globe-visualization');
 const data = JSON.parse(node.dataset.points);
@@ -8,8 +8,8 @@ const data = JSON.parse(node.dataset.points);
 const colorInterpolator = t => `rgba(255, 100, 50, ${ 1 - t })`;
 
 const Globe = new ThreeGlobe()
-	.globeImageUrl('https://unpkg.com/three-globe/example/img/earth-night.jpg')
-	.bumpImageUrl('https://unpkg.com/three-globe/example/img/earth-topology.png')
+	.globeImageUrl('/world/earth-night.jpg')
+	.bumpImageUrl('/world/earth-topology.png')
 	.ringsData(data)
 	.ringColor(() => colorInterpolator)
 	.ringMaxRadius(() => 5)
@@ -41,10 +41,10 @@ camera.updateProjectionMatrix();
 camera.position.z = 150;
 
 // Add camera controls
-const tbControls = new TrackballControls(camera, renderer.domElement);
-tbControls.minDistance = 101;
-tbControls.rotateSpeed = 1;
-tbControls.zoomSpeed = 0.2;
+// const tbControls = new TrackballControls(camera, renderer.domElement);
+// tbControls.minDistance = 101;
+// tbControls.rotateSpeed = 1;
+// tbControls.zoomSpeed = 0.2;
 
 function setSize() {
 	const width = node.clientWidth;
@@ -103,7 +103,7 @@ function interpolate(from, to, tick, ticks) {
 (function animate(timestamp) {
 	tick(timestamp * 0.001);
 	
-	tbControls.update();
+	// tbControls.update();
 	renderer.render(scene, camera);
 	requestAnimationFrame(animate);
 })();
