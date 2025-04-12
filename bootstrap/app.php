a26bdd9492e331c7c8efef3b221dbcc8df47262c
Chris Morrell <inxilpro@users.noreply.github.com>
2025-04-12 18:08:58 -0400

    Force app to assume HTTP behind proxy 😅

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\ForceHttpMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
 	)
 	->withMiddleware(function(Middleware $middleware) {
 		$middleware->replace(TrustProxies::class, TrustCloudflareProxies::class);
+		$middleware->prepend(ForceHttpMiddleware::class);
 	})
 	->withExceptions(function(Exceptions $exceptions) {
 	})->create();
