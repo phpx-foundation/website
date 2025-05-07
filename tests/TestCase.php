41e1e9df9689ba0762137608947c04d3239f9416
Ben Holmen <ben@benholmen.com>
2025-05-06 23:03:19 -0500

    Update seeder + tests to be generic

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,14 +10,6 @@ use PHPUnit\Framework\Attributes\Before;
 abstract class TestCase extends BaseTestCase
 {
 	#[Before]
-	public function seedDefaultGroups(): void
-	{
-		if (method_exists($this, 'refreshDatabase')) {
-			$this->afterApplicationCreated(fn() => $this->seed(GroupSeeder::class));
-		}
-	}
-	
-	#[Before]
 	public function disableVite(): void
 	{
 		$this->afterApplicationCreated(fn() => $this->withoutVite());
