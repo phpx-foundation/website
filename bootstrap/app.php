cdb17c0bdad8f2339c4aca4ee1ed2e8ed0e29dcb
Chris Morrell <inxilpro@users.noreply.github.com>
2025-05-14 08:53:41 -0400

    Add bugsnag

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\TrustProxies;
 use Monicahq\Cloudflare\Http\Middleware\TrustProxies as TrustCloudflareProxies;
 
+(new \Bugsnag\BugsnagLaravel\OomBootstrapper())->bootstrap();
+
 return Application::configure(basePath: dirname(__DIR__))
 	->withRouting(
 		web: __DIR__.'/../routes/web.php',
