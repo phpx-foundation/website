4b2e5d5610f7c3e8324f8cbd88875ef50583f3ca
Chris Morrell <inxilpro@users.noreply.github.com>
2025-09-22 16:25:38 -0400

    Basic organizers resource page

---
+++ app/Filament/Pages/Organizers.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use DaniloPolani\FilamentPlausibleWidget\Widgets\PlausibleWidget;
+use Filament\Actions\Action;
+use Filament\Pages\Page;
+use Illuminate\Support\Facades\Auth;
+
+class Organizers extends Page
+{
+	protected static ?string $navigationIcon = 'heroicon-o-user-group';
+
+	protected static ?string $navigationGroup = 'Resources';
+
+	protected static ?string $title = 'Private Resources';
+
+	protected static ?int $navigationSort = PHP_INT_MIN;
+
+	protected static string $view = 'filament.pages.organizers';
+	
+	public static function canAccess(): bool
+	{
+		return Auth::user()->isAnyGroupAdmin() || Auth::user()->isSuperAdmin();
+	}
+	
+	public function getHeading(): string
+	{
+		return 'Private Organizer Resources';
+	}
+	
+	protected function getViewData(): array
+	{
+		return [
+			'mailcoach_promo_code' => config('phpx.organizer_promo_codes.mailcoach'),
+		];
+	}
+}
